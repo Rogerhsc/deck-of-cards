8873fe46abf331e23088e1018f46c76058dda85e
Roger Conceição <rconceicao@esapiens.com.br>
2022-11-02 09:47:12 -0300

    feat(init Project): roteamento da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Deck from "./views/deck";
+import Home from "./views/home";
 
-export default App;
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/deck" element={<Deck />} />
+    </Routes>
+  </Router>
+);
+
+export default App
